9a2d89252d4e1a71d51ddd30e54476da9af09d94
Tyler Crosse <tylerscottcrosse@gmail.com>
2025-06-08 09:48:31 -0700

    WIP on adding references/citations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,30 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
+import remarkGfm from "remark-gfm";
+import rehypeCitation from "rehype-citation";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://tylercrosse.com",
   integrations: [mdx(), sitemap(), pagefind()],
   markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypeCitation,
+        {
+          bibliography: "src/references.bib",
+          linkCitations: true,
+          showTooltips: true,
+          tooltipAttribute: "data-tooltip",
+        },
+      ],
+    ],
     shikiConfig: {
       themes: {
-        light: 'solarized-light',
-        dark: 'solarized-dark',
+        light: "solarized-light",
+        dark: "solarized-dark",
       },
       wrap: true,
     },
