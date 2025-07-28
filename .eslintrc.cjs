9fd7179152fe0ba8bfb397ee2fe88f07c1454359
Tyler Crosse <tylerscottcrosse@gmail.com>
2025-07-28 12:22:24 -0700

    Add all the WIP concurrency stuff

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+    {
+      files: ["**/*.mdx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      rules: {
+        "semi": "off",
+        "no-undef": "off",
+        "no-unused-vars": "off"
+      }
+    }
+  ],
+}; 
