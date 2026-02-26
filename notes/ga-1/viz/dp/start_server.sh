71c0bc055c1658652fad0d54e5a17be2aaafe537
Tyler Crosse <tylerscottcrosse@gmail.com>
2026-02-26 09:46:21 -0800

    Handle notes

--- notes/ga-1/viz/dp/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start a simple Python HTTP server on port 8000
-echo "Starting local server for DP Visualization..."
-echo "Open http://localhost:8000 in your browser."
-python3 -m http.server 8000 --bind 127.0.0.1
